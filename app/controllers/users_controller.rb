816705c4874afbca31fa41b96addbe4727a038da
nayudesu <srkm.149@gmail.com>
2022-08-14 00:12:21 +0000

    user エラー文追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,8 +16,8 @@ class UsersController < ApplicationController
   end
   
   def update
-    user = User.find(params[:id])
-    if user.update(user_params)
+    @user = User.find(params[:id])
+    if @user.update(user_params)
       redirect_to user_path
     else
       render :edit
